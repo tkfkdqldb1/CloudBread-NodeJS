f65fbe257994e9781ecff49313f39f93f9e9afdd
tkfkdqldb1 <tkfkdqldb1@naver.com>
2017-11-11 18:48:11 +0900

    SelLoginIDDupeCheck implementation

--- CloudBread-Core/routes/login.js
+++ CloudBread-Core/routes/login.js
@@ -1,11 +1,31 @@
 let express = require('express');
+let models = require('../models');
 let router = express.Router();
+
+
+let user = require('./user/user.ctrl');
 
 /* Router for Log In system */
 
 /* Implement message router here */
 router.get("/get", (req, res, next) => {
     res.send("Login GET Test");
+});
+
+router.get("/SelLoginIDDupeCheck", (req, res, next) => {
+    const FindID = String(req.query.memberID);
+
+    models.Member.findAll({
+      where: {MemberID: FindID}
+    }).then((result) => {
+
+    if (result.length === 0) {
+      res.send(true);
+    } else {
+      res.send(false);
+    }
+
+  });
 });
 
 /* if request route that is not implemented */
